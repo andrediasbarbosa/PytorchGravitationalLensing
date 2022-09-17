1a0082a601aa0729e3eda7d16e7ca0c0eb90e24a
André <andre.dias.barbosa@gmail.com>
2022-09-17 14:42:22 +0100

    Baseline commit to Repo

--- LightDeflection.py
+++ LightDeflection.py
@@ -1,11 +1,15 @@
 import matplotlib.pyplot as plt  # a MATLAB-like plotting framework
 import numpy as np  # efficient vector and matrix operations
+from Class_files import point_bh, point_mass
 
 data = {'a': np.arange(50),
         'c': np.random.randint(0, 50, 50),
         'd': np.random.randn(50)}
 data['b'] = data['a'] + 10 * np.random.randn(50)
 data['d'] = np.abs(data['d']) * 100
+
+bh=point_bh(3.0)
+pm=point_mass(3.0)
 
 plt.scatter('a', 'b', c='c', s='d', data=data)
 plt.xlabel('entry a')
